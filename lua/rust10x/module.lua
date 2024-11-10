168594990287ef0d2081cdf2a59ed65fdbf93405
Matti Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2024-11-10 17:57:17 +0100

    Implement basic functionality for pull func

--- lua/rust10x/module.lua
+++ lua/rust10x/module.lua
@@ -2,8 +2,40 @@
 local M = {}
 
 ---@return string
-M.pull_snippets = function(location)
-  return location
+M.pull_snippets = function(snippets_path)
+  local Path = require("plenary.path")
+  local Job = require("plenary.job")
+
+  local snippets_repo_path = "/tmp/rust10x.temp"
+
+  local snippets_repo = Path:new(snippets_repo_path)
+
+  if snippets_repo:exists() then
+    Job:new({
+      command = "git",
+      args = { "pull" },
+      cwd = snippets_repo:absolute(),
+    }):sync()
+  else
+    Job:new({
+      command = "git",
+      args = { "clone", "https://github.com/your-forked-snippets-repo", snippets_repo:absolute() },
+    }):sync()
+  end
+
+  if not snippets_path:exists() then
+    Job:new({
+      command = "mkdir",
+      args = { snippets_path.absolute() },
+    }):sync()
+  end
+
+  Job:new({
+    command = "cp",
+    args = { snippets_repo:absolute() .. "snippets", snippets_path.absolute() },
+  }):sync()
+
+  return snippets_path
 end
 
 return M
